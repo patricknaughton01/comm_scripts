4d7073185643d8392aea5cf2c6c1705dd60bca9b
Patrick Naughton <patricknaughton01@gmail.com>
2018-02-09 16:38:34 -0600

    Broadcasting
    Allow each device to broadcast its message instead of sending directly
    to one other IP address.

--- recv.py
+++ recv.py
@@ -1,6 +1,6 @@
 import socket
 
-UDP_IP = "192.168.1.2"
+UDP_IP = "255.255.255.255"
 UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET,	# Internet
@@ -9,5 +9,5 @@ sock = socket.socket(socket.AF_INET,	# Internet
 sock.bind((UDP_IP, UDP_PORT))
 
 while True:
-	data, addr = sock.recvfrom(1024)
+	data = sock.recv(1024)
 	print("received message: " + data)
--- send.py
+++ send.py
@@ -1,6 +1,6 @@
 import socket
 
-UDP_IP = "192.168.1.2"
+UDP_IP = "255.255.255.255"
 UDP_PORT = 5005
 
 MESSAGE = "HELLO WORLD"
@@ -11,6 +11,9 @@ print("")
 
 sock = socket.socket(socket.AF_INET,	# Internet
 		    socket.SOCK_DGRAM)	# UDP
+
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) # see pubs.opengroup.org/onlinepubs/009695399/functions/setsockopt.html for docs
+
 while True:
 	sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 	MESSAGE = raw_input("Enter a message: ")
