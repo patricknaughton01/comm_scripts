ce94bef94c27beed9c7fe8a6b7ac234dfaf4bf53
Patrick Naughton <patricknaughton01@gmail.com>
2018-02-12 23:09:59 -0600

    Threading example
    Put an example of threading in network.py to experiment. It may be more
    difficult to kill threads than originally anticipated. Will do further
    research.
    This example may be good to put in the docs folder.

--- core/network.py
+++ core/network.py
@@ -1,5 +1,24 @@
 import socket
 import threading
+import time
+
+def main():
+    """ A simple threading example that starts two threads. Each
+    uses a call to time.sleep for a different amount of time so
+    that they print their output asynchronously.
+    
+    """
+    """threads=[]
+    threads.append(threading.Thread(name="thread1", target=target, args=(1, 2)))
+    threads.append(threading.Thread(name="thread2", target=target, args=(2, 3)))
+    for thread in threads:
+        thread.start()
+    pass
+    
+def target(num1, num2):
+    for i in range(4):
+        print("Executing target: " + str(num1) + " on thread: " + threading.currentThread().getName())
+        time.sleep(num2)"""
 
 class Network:
     def __init__(signature, max_packet_length):
@@ -14,3 +33,6 @@ class Network:
         
     def broadcast():
         pass
+        
+if __name__ == "__main__":
+    main()
