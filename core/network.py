1ba0ba716d0424767ec2c7942527865089b35437
Patrick Naughton <patricknaughton01@gmail.com>
2018-02-13 11:00:58 -0600

    Receiving in the Network class
    Started building the receive functions for the network class. Some TODOs
    still to be completed.
    Added a .gitignore to ignore ide specific directories and files. Feel
    free to add to this.

--- core/network.py
+++ core/network.py
@@ -1,38 +1,69 @@
 import socket
-import threading
+import multiprocessing, os
 import time
 
+
 def main():
-    """ A simple threading example that starts two threads. Each
-    uses a call to time.sleep for a different amount of time so
-    that they print their output asynchronously.
+    """ A simple processing example that concurrently runs two delay timed
+    loops that print out information about the process they're running on.
+    Used to allow the network to listen while it sends out information.
     
     """
-    """threads=[]
-    threads.append(threading.Thread(name="thread1", target=target, args=(1, 2)))
-    threads.append(threading.Thread(name="thread2", target=target, args=(2, 3)))
-    for thread in threads:
-        thread.start()
-    pass
-    
+    processes=[]
+    processes.append(multiprocessing.Process(name="thread1", target=target, args=(1, 2)))
+    processes.append(multiprocessing.Process(name="thread2", target=target, args=(2, 3)))
+    print("Parent process id: " + str(os.getpid()))
+    for process in processes:
+        process.start()
+
+
 def target(num1, num2):
     for i in range(4):
-        print("Executing target: " + str(num1) + " on thread: " + threading.currentThread().getName())
-        time.sleep(num2)"""
+        print("Executing target: " + str(num1) + " with process: " + str(os.getpid()) +
+              "\nWhose parent process is: " + str(os.getppid()))
+        time.sleep(num2)
+    os.abort()
+
 
 class Network:
-    def __init__(signature, max_packet_length):
+    def __init__(self, signature, max_packet_length, buffer_size):
+        self.signature = signature
         self.max_packet_length = max_packet_length
+
+        self.unreads = []
+        self.logged_messages = []
+        self.buffer_size = buffer_size
+        self.listening_process = None
+
+        self.broadcast_socket = None
+        self.listen_socket = None
         
+    def start_listening(self, ip, port, connection_type):
+        if self.listen_socket is None:
+            try:
+                self.listen_socket = socket.socket(socket.AF_INET, connection_type)
+                self.listen_socket.bind((ip, port))
+                self.listening_process = multiprocessing.Process(
+                    name="listening_process_"+str(self.signature), target=self.update_messages())
+                self.listening_process.start()
+            except RuntimeError:
+                raise RuntimeError("Listening socket failed to open")
         
-    def start_listening():
+    def stop_listening(self):
+        self.listening_process.abort()
+        self.listen_socket = None
+
+    def update_messages(self):
+        while True:
+            data, addr = self.listen_socket.recvfrom(self.max_packet_length)
+            # TODO: create Message wrapper class from data and append to unreads if there's space for it
+        
+    def broadcast(self):
+        pass
+
+    def close_broadcast(self):
         pass
         
-    def stop_listening():
-        pass
-        
-    def broadcast():
-        pass
-        
+
 if __name__ == "__main__":
     main()
