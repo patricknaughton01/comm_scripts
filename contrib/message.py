90e51a5f3c60c2c45661baf3b2173544fb244213
Patrick Naughton <patricknaughton01@gmail.com>
2018-02-14 10:11:10 -0600

    Potential Message conflicts
    Based on the different things that we want to do with the messages, we
    may need subclasses IncomingMessage and OutgoingMessage.

--- contrib/message.py
+++ contrib/message.py
@@ -3,10 +3,12 @@ import re
 
 
 class Message:
-    def __init__(self, addr_to, addr_from, content, timestamp=False):
+    def __init__(self, addr_to, addr_from, content, timestamp=False, sign=False):
         self.addr_to = addr_to
         self.addr_from = addr_from
         self.content = content
+        self.content += "<to>" + str(self.addr_to) + "</to>"
+        self.content += "<from>" + str(self.addr_from) + "</from>"
         if timestamp:
             self.timestamp = datetime.datetime.now()
             self.timestamp_str = (str(self.timestamp.year)
@@ -16,7 +18,17 @@ class Message:
                                   + "_" + str(self.timestamp.minute)
                                   + "_" + str(self.timestamp.second))
             self.content += "<time_stamp>" + self.timestamp_str + "</time_stamp>"
-        self.content += "<signature>" + str(self.addr_from) + "</signature>"
+        if sign:
+            self.content += "<signature>" + str(self.addr_from) + "</signature>"
+
+    def add_timestamp(self, content):
+        pass
+
+    def add_signature(self, content):
+        pass
+
+    def convert_to_message(self, xml_string):
+        pass
 
     def find_values(self, key, attributes=None):
         #TODO find and return all of the values that match the given key and attributes
