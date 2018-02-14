335b8446dd2cc384194eeebac701a9ea2c6346f9
Patrick Naughton <patricknaughton01@gmail.com>
2018-02-13 18:13:50 -0600

    extra things

---
+++ contrib/message.py
@@ -0,0 +1,24 @@
+import datetime
+
+
+class Message:
+    def __init__(self, addr_to, addr_from, content, timestamp=False):
+        self.addr_to = addr_to
+        self.addr_from = addr_from
+        self.content = content
+        if timestamp:
+            self.timestamp = datetime.datetime.now()
+            self.timestamp_str = (str(self.timestamp.year)
+                                  + "_" + str(self.timestamp.month)
+                                  + "_" + str(self.timestamp.day)
+                                  + "_" + str(self.timestamp.hour)
+                                  + "_" + str(self.timestamp.minute)
+                                  + "_" + str(self.timestamp.second))
+            self.content += "<time_stamp>" + self.timestamp_str + "</time_stamp>"
+        self.content += "<signature>" + str(self.addr_from) + "</signature>"
+
+    def find_value(self, key):
+        pass
+
+    def __str__(self):
+        return self.content
