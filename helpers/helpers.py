a5f9f1c53f0a9ea39c8665503d3f9cb1d7e6a80a
Patrick Naughton <patricknaughton01@gmail.com>
2018-02-18 22:19:53 -0600

    XML Parsing, incoming and outgoing messages
    Created helper functions to parse XML
    	Can now search for specific values given their keys and can
    	specify that they have to have certain attributes.
    
    	Currently configured such that to match, a given key-value pair
    	must have all passed attributes.
    
    Wrote the IncomingMessage class.
    	Tested IncomingMessage class under a few different conditions.
    
    Started OutgoingMessage class.
    	Still need to write tests for it.
    
    Right now, tests are inside each individual class file in a main
    function. That is probably useful for small scale testing, but may need
    to put more comprehensive tests in the tests directory.
    
    Still need to document pretty much every function. Would be a good way
    to learn about what was done.

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -4,3 +4,30 @@ def remove_spaces(str):
         if not str[i].isspace():
             r += str[i]
     return r
+
+
+def get_attributes(str):
+    str = remove_spaces(str)
+    attr_vals = {}
+    markers = []
+    for i in range(len(str)):
+        if str[i] == "'" or str[i] == '"':          # if we come upon a quote
+            if i == 0 or str[i-1] != '\\':          # ignore the quote if it is escaped
+                markers.append(i)
+    if len(markers)%2 != 0:                         # must have an even number of quotes to be valid
+        raise RuntimeError("get_attributes received a string whose quotes don't match up.")
+    for i in range(0, len(markers)-1, 2):
+        val = str[markers[i]+1:markers[i+1]]        # get string between the markers
+        if i == 0:
+            attr = str[:markers[i]-1]               # -1 to get rid of the equals sign (we know there won't be spaces)
+        else:
+            attr = str[markers[i-1]+1:markers[i]-1] # -1 to get rid of quote and equals sign
+        attr_vals.update({attr:val})
+    return attr_vals
+
+
+def find_last(target, str):
+    for i in range(len(str)-1, -1, -1):
+        if str[i] == target:
+            return i
+    return -1
