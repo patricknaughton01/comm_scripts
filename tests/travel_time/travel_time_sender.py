8431fcccd26c0c45d2e8f655a29c47025588aff5
Patrick Naughton <patricknaughton01@gmail.com>
2018-03-21 16:06:09 -0500

    fixup! Timing output file

--- tests/travel_time/travel_time_sender.py
+++ tests/travel_time/travel_time_sender.py
@@ -7,8 +7,10 @@ def main():
     """A simple test to calculate the round trip time of
     packets
 
-    This test was performed on:
-    Results:
+    This test was performed on: 3/21/2018
+    Results: Overwhelmingly successful. See a fuller description
+    on the log page:
+    (https://classes.engineering.wustl.edu/ese205/core/index.php?title=Pi_Car_Comm_Log#Timing_Test_.28Finally.29)
 
     :return: None
     """
