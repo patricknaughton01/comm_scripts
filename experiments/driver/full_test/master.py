4418b17828e8c257acaddc793501157f05eec235
Patrick Naughton <patricknaughton01@gmail.com>
2018-03-30 16:34:20 -0500

    Full test
    Created code for a full test of the system (see if
    we can go from remote to receiver to arduino to pi to
    broadcast to pi to arduino to motor).

--- experiments/driver/full_test/master.py
+++ experiments/driver/full_test/master.py
@@ -4,7 +4,18 @@ from core.network import Network
 
 
 def main():
-    pass
+    ser = serial.Serial(
+        port='/dev/ttyACM0',
+        baudrate=9600,
+        timeout=0,
+    )
+    network = Network(1024, 10)
+    try:
+        while True:
+            command = ser.readline().decode('utf-8')
+            network.broadcast("<command>" + command + "</command>")
+    except KeyboardInterrupt:
+        network.close_broadcast()
 
 
 if __name__ == "__main__":
