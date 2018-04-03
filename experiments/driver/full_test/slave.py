aa49bac6fe487256bb2c7371c8113499cede595b
Patrick Naughton <patricknaughton01@gmail.com>
2018-04-03 11:31:00 -0500

    Documentation

--- experiments/driver/full_test/slave.py
+++ experiments/driver/full_test/slave.py
@@ -6,11 +6,15 @@ from core.network import Network
 
 
 def main():
+    # Open a serial port to give the Arduino commands
     ser = serial.Serial(
         port="/dev/ttyACM0",
         baudrate=9600,
     )
+    # Create a network that can read in packets of length 1024 bytes or less
+    # and that stores the last 10 messages ir received.
     network = Network(1024, 10)
+    # Start listening for messages (UDP packets).
     network.start_listening(socket.SOCK_DGRAM)
     try:
         while True:
